caf56272f3fac0e3a8239f3f17bcd5193090c58e
samuelmo <samuelmo78@gmail.com>
2019-09-07 02:10:24 -0600

    add google map api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component }from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <main>
-      <div id="map"></div>
-    </main>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCuaxwYkJD3zRj5YhNkcnJoYz-xuhK78Lw&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById('map'),
+     {
+       center: {lat: 40.626162, lng: -111.911355},
+       zoom: 16
+     })
+  }
+
+  render() {
+    return (
+      <main>
+        <div id="map"></div>
+      </main>
+    );
+  }
+}
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+
 }
 
 export default App;
