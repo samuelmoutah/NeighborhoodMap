144f06b6eba0d92975e10216469614614b4a0cec
samuelmo <samuelmo78@gmail.com>
2019-09-07 03:05:06 -0600

    get venues from FourSqure API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component }from 'react';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
+    this.getVenues()
     this.renderMap()
   }
 
   renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCuaxwYkJD3zRj5YhNkcnJoYz-xuhK78Lw&callback=initMap")
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyD1DrDBUd6GNL2EIBCxK-K0OjkTny8kbuA&callback=initMap")
     window.initMap = this.initMap
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "U02WBXTTFQTWRV2ZP0SY0E0TAUEZOMDM3H2DH23OOJDLRTT5",
+      client_secret: "OJJIQB20UBKBKBYEXDFMFUAHLXJ2ICEZ441CSXPQ32S5EVCJ",
+      query: "food",
+      near: "Midvale",
+      v: "20182507" 
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        })
+      })
+      .catch(error => {
+        console.log("Error!!" + error)
+      })
   }
 
   initMap = () => {
